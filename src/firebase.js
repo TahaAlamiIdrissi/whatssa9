e237fe9c04f994d966bc6a99089c8a61d6fb331a
Taha Alami Idrissi <3624559-TahaAlamiIdrissi@users.noreply.gitlab.com>
2020-12-25 01:25:42 +0100

    feat : added some lines to the gitignore file

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import firebase  from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBiegiZbjTWwsMw1L_kmKA5LytRDhQEIDg",
-  authDomain: "whatssap-clone-7c4ed.firebaseapp.com",
-  projectId: "whatssap-clone-7c4ed",
-  storageBucket: "whatssap-clone-7c4ed.appspot.com",
-  messagingSenderId: "199300240027",
-  appId: "1:199300240027:web:9290552c903460af2b2c3e",
-  measurementId: "G-6NL4LP64SC"
+  // get the data from firebase 
 };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
